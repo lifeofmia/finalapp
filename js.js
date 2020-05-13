db31de143c15c562b7d2c7d5090c17d76d31b0ee
Mia Villanueva <miavillanueva@Mias-MacBook-Air.local>
2020-05-12 18:09:17 -0700

    js

--- js.js
+++ js.js
@@ -1,2 +1,12 @@
-let x=0
-let jomama= mia-hi
+var button = document.getElementById('changeBackground');
+
+function backgroundChange() {
+  var Color = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = Color;
+}
+
+function random(num) {
+    return Math.floor(Math.random()*(num+1));
+}
+
+button.addEventListener('click', backgroundChange);
